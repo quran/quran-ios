23820de4e955d5177283b9676858e2dcbd18c0c3
Mohamed Afifi <mohamede1945@gmail.com>
2016-04-30 15:32:24 +0200

    App icon

--- Quran/BasePageSelectionViewController.swift
+++ Quran/BasePageSelectionViewController.swift
@@ -6,4 +6,66 @@
 //  Copyright © 2016 Quran.com. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import GenericDataSources
+
+class BasePageSelectionViewController<ItemType, CellType: ReusableCell>: UIViewController {
+
+    let dataRetriever: AnyDataRetriever<[(Juz, [ItemType])]>
+    let dataSource = JuzsMutlipleSectionDataSource(type: .MultiSection, headerReuseIdentifier: "header")
+
+    init(dataRetriever: AnyDataRetriever<[(Juz, [ItemType])]>) {
+        self.dataRetriever = dataRetriever
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+
+    let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpViews()
+
+        navigationItem.titleView = UIImageView(image: UIImage(named: "logo-22")?.imageWithRenderingMode(.AlwaysTemplate))
+        
+        tableView.sectionHeaderHeight = 44
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 70
+
+        tableView.registerClass(JuzTableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "header")
+
+        tableView.ds_useDataSource(dataSource)
+
+        dataRetriever.retrieve { [weak self] (data: [(Juz, [ItemType])]) in
+
+            guard let `self` = self else {
+                return
+            }
+
+            self.dataSource.setSections(data) { self.createItemsDataSource() }
+        }
+    }
+
+    private func setUpViews() {
+        view.addAutoLayoutSubview(tableView)
+        view.pinParentAllDirections(tableView)
+
+        let statusView = UIView()
+        statusView.backgroundColor = UIColor.appIdentity()
+        view.addAutoLayoutSubview(statusView)
+        view.pinParentHorizontal(statusView)
+        view.addParentTopConstraint(statusView)
+        statusView.addHeightConstraint(20)
+    }
+
+    func createItemsDataSource() -> BasicDataSource<ItemType, CellType> {
+        fatalError("Should be implemented by subclasses")
+    }
+}
--- Quran/SurasViewController.swift
+++ Quran/SurasViewController.swift
@@ -9,55 +9,18 @@
 import UIKit
 import GenericDataSources
 
-class SurasViewController: UIViewController {
+class SurasViewController: BasePageSelectionViewController<Sura, SuraTableViewCell> {
 
-    let dataRetriever: AnyDataRetriever<[(Juz, [Sura])]>
-    let dataSource = JuzsMutlipleSectionDataSource(type: .MultiSection, headerReuseIdentifier: "header")
-
-    init(dataRetriever: AnyDataRetriever<[(Juz, [Sura])]>) {
-        self.dataRetriever = dataRetriever
-        super.init(nibName: nil, bundle: nil)
+    override init(dataRetriever: AnyDataRetriever<[(Juz, [Sura])]>) {
+        super.init(dataRetriever: dataRetriever)
     }
 
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    override func createItemsDataSource() -> BasicDataSource<Sura, SuraTableViewCell> {
+        return SurasDataSource(reuseIdentifier: "cell")
     }
-
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return .LightContent
-    }
-
-    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpViews()
-
-        title = NSBundle.mainBundle().localizedInfoDictionary?["CFBundleName"] as? String ?? ""
-        view.backgroundColor = UIColor.secondaryColor()
-        tableView.sectionHeaderHeight = 44
-        tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = 70
-
         tableView.registerNib(UINib(nibName: "SuraTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
-        tableView.registerClass(JuzTableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "header")
-
-        tableView.ds_useDataSource(dataSource)
-
-        dataRetriever.retrieve { [weak self] data in
-            self?.dataSource.setSections(data) { SurasDataSource(reuseIdentifier: "cell") }
-        }
-    }
-
-    private func setUpViews() {
-        view.addAutoLayoutSubview(tableView)
-        view.pinParentAllDirections(tableView)
-
-        let statusView = UIView()
-        statusView.backgroundColor = UIColor.appIdentity()
-        view.addAutoLayoutSubview(statusView)
-        view.pinParentHorizontal(statusView)
-        view.addParentTopConstraint(statusView)
-        statusView.addHeightConstraint(20)
     }
 }

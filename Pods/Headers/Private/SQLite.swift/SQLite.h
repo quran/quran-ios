9bf87bfb41c77ed26b70568c67216a96ce88e2c1
ahmedre <ahmedre@gmail.com>
2016-05-12 20:44:31 -0700

    Import SQLite.swift
    This is just the result of adding the SQLite.swift pod and then running
    pod install.

---
+++ Pods/Headers/Private/SQLite.swift/SQLite.h
@@ -0,0 +1,1 @@
+../../../SQLite.swift/SQLite/SQLite.h
---
+++ Pods/Target Support Files/SQLite.swift/SQLite.swift-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "SQLite-Bridging.h"
+#import "SQLite.h"
+
+FOUNDATION_EXPORT double SQLiteVersionNumber;
+FOUNDATION_EXPORT const unsigned char SQLiteVersionString[];
+

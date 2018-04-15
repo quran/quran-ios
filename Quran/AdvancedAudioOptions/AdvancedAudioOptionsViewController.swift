092e3151ae7db53779ba1acf20db6d9711df9291
Mohamed Afifi <mohamede1945@gmail.com>
2018-04-15 19:56:48 -0400

    Fix swiftlint header violations

--- Quran/AdvancedAudioOptions/AdvancedAudioOptionsViewController.swift
+++ Quran/AdvancedAudioOptions/AdvancedAudioOptionsViewController.swift
@@ -1,9 +1,21 @@
 //
 //  AdvancedAudioOptionsViewController.swift
-//  AudioCards
+//  Quran
 //
 //  Created by Afifi, Mohamed on 2018-04-07.
-//  Copyright © 2018 Afifi, Mohamed. All rights reserved.
+//
+//  Quran for iOS is a Quran reading application for iOS.
+//  Copyright (C) 2018  Quran.com
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
 //
 import GenericDataSources
 import QueuePlayer

710fc6050d674eb1e173a7ea71f54fbc3c7dcbc4
Mohamed Afifi <mohamede1945@gmail.com>
2018-09-06 18:24:17 -0400

    162: Search by ayah number or sura number

--- Quran/SearchService.swift
+++ Quran/SearchService.swift
@@ -28,6 +28,11 @@ protocol SearchAutocompletionService {
 
 class SQLiteSearchService: SearchAutocompletionService, SearchService {
 
+    private let numberParser: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.locale = Locale(identifier: "en")
+        return formatter
+    }()
     private let localTranslationInteractor: AnyGetInteractor<[TranslationFull]>
     private let arabicPersistence: AyahTextPersistence
     private let translationPersistenceCreator: AnyCreator<String, AyahTextPersistence>
@@ -67,22 +72,30 @@ class SQLiteSearchService: SearchAutocompletionService, SearchService {
     func search(for term: String) -> Observable<SearchResults> {
         let translationCreator = self.translationPersistenceCreator
         let arabicPersistence = self.arabicPersistence
+        let ayahNumber = self.isAyahNumberSearch(term)
 
         return prepare()
             .map { translations -> SearchResults in
-                let arabicResults = try arabicPersistence.search(for: term)
-                guard arabicResults.isEmpty else {
-                    return SearchResults(source: .quran, items: arabicResults)
-                }
-                for translation in translations {
-                    let fileURL = Files.translationsURL.appendingPathComponent(translation.fileName)
-                    let persistence = translationCreator.create(fileURL.absoluteString)
-                    let results = try persistence.search(for: term)
-                    if !results.isEmpty {
-                        return SearchResults(source: .translation(translation), items: results)
+
+                if let ayah = ayahNumber {
+                    let ayahText = try arabicPersistence.getAyahTextForNumber(ayah)
+                    let result = SearchResult(text: ayahText, ayah: ayah, page: Quran.pageForAyah(ayah))
+                    return SearchResults(source: .quran, items: [result])
+                } else {
+                    let arabicResults = try arabicPersistence.search(for: term)
+                    guard arabicResults.isEmpty else {
+                        return SearchResults(source: .quran, items: arabicResults)
                     }
+                    for translation in translations {
+                        let fileURL = Files.translationsURL.appendingPathComponent(translation.fileName)
+                        let persistence = translationCreator.create(fileURL.absoluteString)
+                        let results = try persistence.search(for: term)
+                        if !results.isEmpty {
+                            return SearchResults(source: .translation(translation), items: results)
+                        }
+                    }
+                    return SearchResults(source: .none, items: [])
                 }
-                return SearchResults(source: .none, items: [])
             }
     }
 
@@ -96,5 +109,40 @@ class SQLiteSearchService: SearchAutocompletionService, SearchService {
                     .filter { $0.isDownloaded }
                     .map { $0.translation }
             }
+    }
+
+    private func isAyahNumberSearch(_ term: String) -> AyahNumber? {
+        let components = term.components(separatedBy: ":")
+        guard !components.isEmpty && components.count <= 2 else {
+            return nil
+        }
+        guard let sura = parseInt(components[0]) else {
+            return nil
+        }
+        guard sura >= Quran.QuranSurasRange.lowerBound && sura <= Quran.QuranSurasRange.upperBound else {
+            return nil
+        }
+        if components.count == 1 {
+            return AyahNumber(sura: sura, ayah: 1)
+        } else {
+            guard let ayah = parseInt(components[1]) else {
+                return nil
+            }
+            guard ayah > 0 && ayah <= Quran.numberOfAyahsForSura(sura) else {
+                return nil
+            }
+            return AyahNumber(sura: sura, ayah: ayah)
+        }
+    }
+
+    private func parseInt(_ value: String) -> Int? {
+        guard let number = numberParser.number(from: value) else {
+            return nil
+        }
+        switch CFNumberGetType(number) {
+        case .charType, .intType, .nsIntegerType, .shortType, .sInt8Type, .sInt16Type, .sInt32Type, .sInt64Type:
+            return number.intValue
+        default: return nil
+        }
     }
 }

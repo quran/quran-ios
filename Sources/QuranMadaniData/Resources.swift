cebc67f83fed9c16f7b5e5df4971a7aece8e1909
Mohamed Afifi <mohamede1945@gmail.com>
2022-01-02 22:32:41 -0500

    Creating QuranMadaniData library. Initially hosting words.db

---
+++ Sources/QuranMadaniData/Resources.swift
@@ -0,0 +1,10 @@
+//
+//  Resources.swift
+//
+//
+//  Created by Mohamed Afifi on 2022-01-02.
+//
+
+import Foundation
+
+public let wordsDatabase = Bundle.module.url(forResource: "words", withExtension: "db")!

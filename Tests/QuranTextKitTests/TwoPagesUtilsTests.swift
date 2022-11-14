1eee2660ddac6ab412b1362e587846386c5816bd
Zubair Khan <zubair.k.khan@oracle.com>
2022-11-14 06:07:07 -0600

    Implement logic to check for whether screen size can support 2 pages

---
+++ Tests/QuranTextKitTests/TwoPagesUtilsTests.swift
@@ -0,0 +1,20 @@
+//
+//  TwoPagesUtilsTests.swift
+//  
+//
+//  Created by Zubair Khan on 2022-11-12.
+//
+
+@testable import QuranTextKit
+import XCTest
+
+class TwoPagesUtilsTests: XCTestCase {
+    func testHasEnoughHorizontalSpace() throws {
+        let hasEnoughSpace = TwoPagesUtils.hasEnoughHorizontalSpace()
+        if UIScreen.main.bounds.width > 900 {
+            XCTAssertTrue(hasEnoughSpace)
+        } else {
+            XCTAssertFalse(hasEnoughSpace)
+        }
+    }
+}

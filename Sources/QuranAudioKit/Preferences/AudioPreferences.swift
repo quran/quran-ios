68fb673b8d242be6aababa282bcfdf7604ea3155
Zubair Khan <zubair.k.khan@oracle.com>
2022-12-06 14:13:56 -0600

    Add support for 'Recent Reciters' feature

--- Sources/QuranAudioKit/Preferences/AudioPreferences.swift
+++ Sources/QuranAudioKit/Preferences/AudioPreferences.swift
@@ -7,19 +7,29 @@
 
 import Combine
 import Foundation
+import OrderedCollections
 import Preferences
 
 @available(iOS 13.0, *)
 public class AudioPreferences {
     public static let shared = AudioPreferences()
+
     private init() {}
 
     private static let lastSelectedReciterId = PreferenceKey<Int>(key: "LastSelectedQariId", defaultValue: 41)
     private static let audioEndKey = PreferenceKey<Int>(key: "audioEndKey", defaultValue: AudioEnd.juz.rawValue)
+    private static let recentReciterIds = PreferenceKey<[Int]>(key: "recentRecitersIdsKey", defaultValue: [])
+    private static let recentReciterIdsTransfomer = PreferenceTransformer<[Int], OrderedSet<Int>>(
+        rawToValue: { OrderedSet($0) },
+        valueToRaw: { Array($0) }
+    )
 
     @TransformedPreference(audioEndKey, transformer: .rawRepresentable(defaultValue: .juz))
     public var audioEnd: AudioEnd
 
     @Preference(lastSelectedReciterId)
     public var lastSelectedReciterId: Int
+
+    @TransformedPreference(recentReciterIds, transformer: recentReciterIdsTransfomer)
+    public var recentReciterIds: OrderedSet<Int>
 }

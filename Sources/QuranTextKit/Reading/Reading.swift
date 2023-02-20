6d84d0cfb7a7343e1cd811d4a55877750641b4ac
Mohamed Afifi <mohamede1945@gmail.com>
2023-02-19 23:27:23 -0500

    Adding localization for hafs 1405 AH mushaf

--- Sources/QuranTextKit/Reading/Reading.swift
+++ Sources/QuranTextKit/Reading/Reading.swift
@@ -9,20 +9,4 @@ import Foundation
 
 public enum Reading: Int, CaseIterable {
     case hafs_1405 = 0
-
-    public var title: String {
-        "Title 1"
-    }
-
-    public var description: String {
-        "Description 1"
-    }
-
-    public var properties: [String] {
-        ["Property 1", "Property 2"]
-    }
-
-    public var imageName: String {
-        "logo-lg-w"
-    }
 }

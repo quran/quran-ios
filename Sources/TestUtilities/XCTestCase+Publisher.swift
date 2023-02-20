71fff2c83ea2209f0f65c99748b9d38a16edf4f0
Mohamed Afifi <mohamede1945@gmail.com>
2023-02-20 12:59:01 -0500

    Implementing ReadingResourcesService

---
+++ Sources/TestUtilities/XCTestCase+Publisher.swift
@@ -0,0 +1,58 @@
+//
+//  XCTestCase+Publisher.swift
+//  
+//
+//  Created by Mohamed Afifi on 2023-02-20.
+//
+
+import Combine
+import XCTest
+
+extension XCTestCase {
+    public func awaitPublisher<T: Publisher>(
+        _ publisher: T,
+        timeout: TimeInterval = 10,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) throws -> T.Output {
+        // This time, we use Swift's Result type to keep track
+        // of the result of our Combine pipeline:
+        var result: Result<T.Output, Error>?
+        let expectation = self.expectation(description: "Awaiting publisher")
+
+        let cancellable = publisher.sink(
+            receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    result = .failure(error)
+                case .finished:
+                    break
+                }
+
+                expectation.fulfill()
+            },
+            receiveValue: { value in
+                result = .success(value)
+            }
+        )
+
+        // Just like before, we await the expectation that we
+        // created at the top of our test, and once done, we
+        // also cancel our cancellable to avoid getting any
+        // unused variable warnings:
+        waitForExpectations(timeout: timeout)
+        cancellable.cancel()
+
+        // Here we pass the original file and line number that
+        // our utility was called at, to tell XCTest to report
+        // any encountered errors at that original call site:
+        let unwrappedResult = try XCTUnwrap(
+            result,
+            "Awaited publisher did not produce any output",
+            file: file,
+            line: line
+        )
+
+        return try unwrappedResult.get()
+    }
+}
---
+++ Tests/QuranTextKitTests/ReadingResourcesServiceTests.swift
@@ -0,0 +1,100 @@
+//
+//  ReadingResourcesServiceTests.swift
+//  
+//
+//  Created by Mohamed Afifi on 2023-02-20.
+//
+
+@testable import QuranTextKit
+import TestUtilities
+import XCTest
+
+final class ReadingResourcesServiceTests: XCTestCase {
+    private var service: ReadingResourcesService!
+
+    override func setUp() {
+        ReadingPreferences.shared.reading = .hafs_1405
+        OnDemandResource.requestInitializer = BundleResourceRequestFake.init
+    }
+
+    func testResourceAvailable() throws {
+        BundleResourceRequestFake.resourceAvailable = true
+        let service = ReadingResourcesService()
+
+        let publisher = service.publisher.collect(1).first()
+        let events = try awaitPublisher(publisher)
+
+        XCTAssertEqual(events, [.ready])
+    }
+
+    func testResourceDownloading() throws {
+        BundleResourceRequestFake.resourceAvailable = false
+        BundleResourceRequestFake.downloadResult = .success(())
+        let service = ReadingResourcesService()
+
+        let publisher = service.publisher.collect(3).first()
+        let events = try awaitPublisher(publisher)
+
+        XCTAssertEqual(events, [.downloading(progress: 0),
+                                .downloading(progress: 1),
+                                .ready])
+    }
+
+    func testResourceDownloadFailure() throws {
+        let error = URLError(.notConnectedToInternet)
+        BundleResourceRequestFake.resourceAvailable = false
+        BundleResourceRequestFake.downloadResult = .failure(error)
+        let service = ReadingResourcesService()
+
+        let publisher = service.publisher.collect(2).first()
+        let events = try awaitPublisher(publisher)
+
+        XCTAssertEqual(events, [.downloading(progress: 0),
+                                .error(error as NSError)])
+    }
+
+    func testResourceSwitching() throws {
+        BundleResourceRequestFake.resourceAvailable = true
+        let service = ReadingResourcesService()
+
+        let publisher = service.publisher.collect(1).first()
+        let events = try awaitPublisher(publisher)
+        XCTAssertEqual(events, [.ready])
+
+        // Switch preference
+        BundleResourceRequestFake.resourceAvailable = false
+        BundleResourceRequestFake.downloadResult = .success(())
+        ReadingPreferences.shared.reading = .hafs_1440
+
+        let newPublisher = service.publisher.collect(4).first()
+        let newEvents = try awaitPublisher(newPublisher)
+        XCTAssertEqual(newEvents, [events.last,
+                                   .downloading(progress: 0),
+                                   .downloading(progress: 1),
+                                   .ready])
+    }
+}
+
+private class BundleResourceRequestFake: NSBundleResourceRequest {
+    static var resourceAvailable: Bool = true
+    static var downloadResult: Result<Void, Error>?
+
+    override func conditionallyBeginAccessingResources(completionHandler: @escaping (Bool) -> Void) {
+        DispatchQueue.global().async {
+            completionHandler(Self.resourceAvailable)
+        }
+    }
+
+    override func beginAccessingResources(completionHandler: @escaping (Error?) -> Void) {
+        DispatchQueue.global().async {
+            switch Self.downloadResult! {
+            case .success:
+                self.progress.completedUnitCount = self.progress.totalUnitCount
+                completionHandler(nil)
+            case .failure(let error):
+                completionHandler(error)
+            }
+        }
+    }
+
+}

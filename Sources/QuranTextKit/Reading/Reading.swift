a6e080ad0156223661bffc59dcfc3fb27682d943
Mohamed Afifi <mohamede1945@gmail.com>
2023-04-15 07:11:00 -0400

    Add Hafs Madina 1421 Mushaf

--- Sources/QuranTextKit/Reading/Reading.swift
+++ Sources/QuranTextKit/Reading/Reading.swift
@@ -8,16 +8,23 @@
 import Foundation
 import QuranKit
 
-public enum Reading: Int, CaseIterable {
+public enum Reading: Int {
     case hafs_1405 = 0
     case hafs_1440 = 1
     case tajweed = 2
+    case hafs_1421 = 3
+
+    public static let sortedReadings: [Reading] = [
+        .hafs_1405, .hafs_1421, .hafs_1440, .tajweed
+    ]
 
     var resourcesTag: String {
         switch self {
         case .hafs_1405: return "hafs_1405"
         case .hafs_1440: return "hafs_1440"
+        case .hafs_1421: return "hafs_1421"
         case .tajweed: return "tajweed"
+
         }
     }
 
@@ -26,6 +33,8 @@ public enum Reading: Int, CaseIterable {
         case .hafs_1405:
             return .hafsMadani1405
         case .hafs_1440:
+            return .hafsMadani1440
+        case .hafs_1421:
             return .hafsMadani1440
         case .tajweed:
             return .hafsMadani1405

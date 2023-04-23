4547eea2e7310df890ac51b6c127d36590034b9c
Mohamed Afifi <mohamede1945@gmail.com>
2023-04-23 13:37:10 -0400

    Make SuraHeaderLocation and AyahNumberLocation init public

--- Sources/QuranTextKit/WordFrame/PageMarkers.swift
+++ Sources/QuranTextKit/WordFrame/PageMarkers.swift
@@ -25,6 +25,14 @@ public struct SuraHeaderLocation {
     let width: Int
     let height: Int
 
+    public init(sura: Sura, x: Int, y: Int, width: Int, height: Int) {
+        self.sura = sura
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    }
+
     public var rect: CGRect {
         CGRect(x: x, y: y - height / 2, width: width, height: height)
     }
@@ -34,6 +42,12 @@ public struct AyahNumberLocation {
     public let ayah: AyahNumber
     let x: Int
     let y: Int
+
+    public init(ayah: AyahNumber, x: Int, y: Int) {
+        self.ayah = ayah
+        self.x = x
+        self.y = y
+    }
 
     public func rect(ofLength length: CGFloat) -> CGRect {
         CGRect(x: CGFloat(x) - length / 2, y: CGFloat(y) - length / 2, width: length, height: length)

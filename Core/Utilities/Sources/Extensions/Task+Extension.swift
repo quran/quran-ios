05142a8077cdc1c850f97228c601fff3315453ce
Mohamed Afifi <mohamede1945@users.noreply.github.com>
2023-06-13 20:54:41 -0400

    Remove PromiseKit extension (#508)

--- Core/Utilities/Sources/Extensions/Task+Extension.swift
+++ Core/Utilities/Sources/Extensions/Task+Extension.swift
@@ -5,6 +5,7 @@
 //  Created by Mohamed Afifi on 2023-05-02.
 //
 
+import Combine
 import Foundation
 
 public final class CancellableTask: Hashable {
@@ -36,5 +37,22 @@ extension Task {
 extension AsyncSequence {
     public func collect() async rethrows -> [Element] {
         try await reduce(into: [Element]()) { $0.append($1) }
+    }
+}
+
+extension Publisher {
+    public func asyncMap<T>(
+        _ transform: @escaping (Output) async -> T
+    ) -> some Publisher<T, Failure> {
+        map { value in
+            Future { promise in
+                Task {
+                    let output = await transform(value)
+                    promise(.success(output))
+                }
+            }
+        }
+        .switchToLatest()
+        .eraseToAnyPublisher()
     }
 }

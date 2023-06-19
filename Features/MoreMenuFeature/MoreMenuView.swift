e266d111347c7dda4b662127b6e346a249d0e409
Mohamed Afifi <mohamede1945@gmail.com>
2023-06-19 19:42:41 -0400

    Move MoreMenuFeature to QuranEngine

---
+++ Features/MoreMenuFeature/MoreMenuView.swift
@@ -0,0 +1,45 @@
+//
+//  MoreMenuView.swift
+//
+//
+//  Created by Afifi, Mohamed on 9/6/21.
+//
+
+import NoorUI
+import SwiftUI
+import UIKit
+
+class MoreMenuView: UIHostingController<NoorUI.MoreMenuView> {
+    // MARK: Lifecycle
+
+    init(viewModel: MoreMenuViewModel) {
+        self.viewModel = viewModel
+        super.init(rootView: .init(store: viewModel.store))
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Internal
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = nil
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view.backgroundColor = nil
+    }
+
+    override func preferredContentSizeDidChange(forChildContentContainer container: UIContentContainer) {
+        super.preferredContentSizeDidChange(forChildContentContainer: container)
+        preferredContentSize = container.preferredContentSize
+    }
+
+    // MARK: Private
+
+    private let viewModel: MoreMenuViewModel
+}

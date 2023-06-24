85736c425a6b79977b721e131297083e4c7a5cd7
Mohamed Afifi <mohamede1945@gmail.com>
2023-06-24 11:43:27 -0400

    Remove the logo image as it's unused

---
+++ UI/NoorUI/Miscellaneous/TestResource+Path.swift
@@ -0,0 +1,12 @@
+//
+//  TestResource+Path.swift
+//
+//
+//  Created by Mohamed Afifi on 2023-06-24.
+//
+
+import Foundation
+
+func testResourceURL(_ resource: String) -> URL {
+    URL(validURL: "\(#filePath)/../../../../Domain/TestResources/test_data/\(resource)").standardized
+}

3d93a2a57aaa2aa4e98123ee86b95fde82e22f16
Mohamed Afifi <mohamede1945@gmail.com>
2023-06-24 21:06:33 -0400

    Create example QuranEngine app

---
+++ Example/QuranEngineApp/Classes/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  QuranEngineApp
+//
+//  Created by Mohamed Afifi on 2023-06-24.
+//
+
+import AppStructureFeature
+import Logging
+import NoorFont
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: Internal
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FontName.registerFonts()
+        LoggingSystem.bootstrap(StreamLogHandler.standardError)
+
+        // Eagerly load download manager to handle any background downloads.
+        Task { _ = await downloadManagerContainer.downloadManager() }
+
+        // Begin fetching resources immediately upon the application's start-up.
+        Task { await container.readingResources.startLoadingResources() }
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+
+    func application(
+        _ application: UIApplication,
+        handleEventsForBackgroundURLSession identifier: String,
+        completionHandler: @escaping () -> Void
+    ) {
+        Task { @MainActor in
+            let downloadManager = await downloadManagerContainer.downloadManager()
+            downloadManager.setBackgroundSessionCompletion(completionHandler)
+        }
+    }
+
+    // MARK: Private
+
+    private let container = Container.shared
+    private let downloadManagerContainer = DownloadManagerContainer.shared
+}
---
+++ Example/QuranEngineApp/Classes/Container.swift
@@ -0,0 +1,110 @@
+//
+//  Container.swift
+//  QuranEngineApp
+//
+//  Created by Mohamed Afifi on 2023-06-24.
+//
+
+import Analytics
+import AnnotationsService
+import AppDependencies
+import BatchDownloader
+import CoreDataModel
+import CoreDataPersistence
+import Foundation
+import LastPagePersistence
+import NotePersistence
+import PageBookmarkPersistence
+import Preferences
+import QuranAudioKit
+import QuranKit
+import QuranTextKit
+import ReadingService
+import TranslationService
+
+actor DownloadManagerContainer {
+    // MARK: Lifecycle
+
+    private init() {}
+
+    // MARK: Internal
+
+    static let shared = DownloadManagerContainer()
+
+    func downloadManager() async -> DownloadManager {
+        if let downloader {
+            return downloader
+        }
+        let downloader = await buildDownloadManager()
+        self.downloader = downloader
+        return downloader
+    }
+
+    // MARK: Private
+
+    private var downloader: DownloadManager?
+
+    private nonisolated func buildDownloadManager() async -> DownloadManager {
+        let configuration = URLSessionConfiguration.background(withIdentifier: "DownloadsBackgroundIdentifier")
+        configuration.timeoutIntervalForRequest = 60 * 5 // 5 minutes
+        return await DownloadManager(
+            maxSimultaneousDownloads: 600,
+            configuration: configuration,
+            downloadsURL: Constant.databasesURL.appendingPathComponent("downloads.db", isDirectory: false)
+        )
+    }
+}
+
+/// Hosts singleton dependencies
+class Container: AppDependencies {
+    // MARK: Lifecycle
+
+    private init() {}
+
+    // MARK: Internal
+
+    static let shared = Container()
+
+    let readingResources = ReadingResourcesService()
+    let analytics: AnalyticsLibrary = LoggingAnalyticsLibrary()
+
+    private(set) lazy var lastPagePersistence: LastPagePersistence = CoreDataLastPagePersistence(stack: coreDataStack)
+    private(set) lazy var pageBookmarkPersistence: PageBookmarkPersistence = CoreDataPageBookmarkPersistence(stack: coreDataStack)
+    private(set) lazy var notePersistence: NotePersistence = CoreDataNotePersistence(stack: coreDataStack)
+
+    var databasesURL: URL { Constant.databasesURL }
+    var wordsDatabase: URL { Constant.wordsDatabase }
+    var filesAppHost: URL { Constant.filesAppHost }
+    var appHost: URL { Constant.appHost }
+
+    var supportsCloudKit: Bool { false }
+
+    func downloadManager() async -> DownloadManager {
+        let container = DownloadManagerContainer.shared
+        return await container.downloadManager()
+    }
+
+    // MARK: Private
+
+    private lazy var coreDataStack: CoreDataStack = {
+        let stack = CoreDataStack(name: "Quran", modelUrl: CoreDataModelResources.quranModel) {
+            let lastPage = CoreDataLastPageUniquifier()
+            let pageBookmark = CoreDataPageBookmarkUniquifier()
+            let note = CoreDataNoteUniquifier()
+            return [lastPage, pageBookmark, note]
+        }
+        return stack
+    }()
+}
+
+private enum Constant {
+    static let wordsDatabase = Bundle.main
+        .url(forResource: "words", withExtension: "db")!
+
+    static let appHost: URL = URL(validURL: "https://quran.app/")
+
+    static let filesAppHost: URL = URL(validURL: "https://files.quran.app/")
+
+    static let databasesURL = FileManager.documentsURL
+        .appendingPathComponent("databases", isDirectory: true)
+}

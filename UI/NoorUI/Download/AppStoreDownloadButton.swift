7de3850a9ddf9482cbe8ed761d9e6c1b9884053b
Mohamed Afifi <mohamede1945@gmail.com>
2023-06-29 23:18:43 -0400

    Create SwiftUI AppStoreDownloadButton

---
+++ UI/NoorUI/Download/AppStoreDownloadButton.swift
@@ -0,0 +1,97 @@
+//
+//  AppStoreDownloadButton.swift
+//
+//
+//  Created by Mohamed Afifi on 2023-06-28.
+//
+
+import SwiftUI
+import UIx
+
+public enum DownloadType {
+    case pending
+    case downloading(progress: Double)
+    case download
+}
+
+struct AppStoreDownloadButton: View {
+    @ScaledMetric private var length = 26
+
+    let type: DownloadType
+    let action: AsyncAction
+
+    var body: some View {
+        Button(asyncAction: action) {
+            Group {
+                switch type {
+                case .pending:
+                    CircularPendingView()
+                case .downloading(let progress):
+                    CircularDownloadingView(progress: progress)
+                case .download:
+                    NoorImage.download.image
+                        .renderingMode(.template)
+                        .foregroundColor(.accentColor)
+                }
+            }
+            .frame(width: length, height: length)
+        }
+        .buttonStyle(.borderless)
+    }
+}
+
+struct CircularPendingView: View {
+    // MARK: Internal
+
+    var body: some View {
+        Arc(circlePercentage: 0.94)
+            .stroke(Color.accentColor, lineWidth: lineWidth)
+            .rotationEffect(.degrees(rotationAngle))
+            .animation(.linear(duration: 1).repeatForever(autoreverses: false))
+            .onAppear {
+                rotationAngle = 360
+            }
+    }
+
+    // MARK: Private
+
+    @State private var rotationAngle: Double = 0
+    @ScaledMetric private var lineWidth: Double = 1
+}
+
+struct CircularDownloadingView: View {
+    @ScaledMetric private var stopLength = 8
+    @ScaledMetric private var lineWidth: Double = 1
+    @ScaledMetric private var fillLineWidth: Double = 4
+
+    let progress: Double
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(Color.accentColor, lineWidth: lineWidth)
+
+            Arc(circlePercentage: progress)
+                .strokeBorder(Color.accentColor, style: StrokeStyle(lineWidth: fillLineWidth, lineCap: .butt))
+
+            Rectangle()
+                .fill(Color.accentColor)
+                .frame(width: stopLength, height: stopLength)
+        }
+    }
+}
+
+struct AppStoreDownloadButton_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            Group {
+                AppStoreDownloadButton(type: .pending) { }
+                AppStoreDownloadButton(type: .downloading(progress: 0.3)) { }
+                AppStoreDownloadButton(type: .download) { }
+                AppStoreDownloadButton(type: .downloading(progress: 0.9)) { }
+            }
+            .padding()
+            .border(Color.red)
+        }
+    }
+}

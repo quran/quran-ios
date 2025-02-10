ff6a92ba576f682f5e97f52dd3061e69424fa99b
Mohannad A. Hassan <mohannad.hassan.90@gmail.com>
2025-02-10 15:13:51 +0200

    Create GRDBPageBookmarkMutationsPersistence

---
+++ Data/PageBookmarkMutationsPersistence/Sources/GRDBPageBookmarkMutationsPersistence.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  QuranEngine
+//
+//  Created by Mohannad Hassan on 10/02/2025.
+//
+
+import Foundation
+
+struct GRDBPageBookmarkMutationsPersistence: PageBookmarkMutationsPersistence {
+
+}
+

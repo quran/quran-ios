341c8d2c18c7b48698553b804a25ac7733629b0c
Mohannad A. Hassan <mohannad.hassan.90@gmail.com>
2025-02-10 14:58:07 +0200

    Create MutatedPageBookmarkModel

---
+++ Data/PageBookmarkMutationsPersistence/Sources/MutatedPageBookmarkModel.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  QuranEngine
+//
+//  Created by Mohannad Hassan on 10/02/2025.
+//
+
+import Foundation
+
+struct MutatedPageBookmarkModel {
+    let remoteID: String?
+    let page: Int
+    let modificationDate: Date
+    let deleted: Bool
+}
--- Data/PageBookmarkMutationsPersistence/Sources/PageBookmarkMutationsPersistence.swift
+++ Data/PageBookmarkMutationsPersistence/Sources/PageBookmarkMutationsPersistence.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 protocol PageBookmarkMutationsPersistence {
-    
+
 }

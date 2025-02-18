299feed9b2a25c0f8448a52531c9c92d8a233bdb
Mohannad A. Hassan <mohannad.hassan.90@gmail.com>
2025-02-18 05:53:56 +0200

    Create SynchronizedPageBookmarkPersistenceTests

---
+++ Data/PageBookmarkPersistence/Tests/SynchronizedPageBookmarkPersistenceTests.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  QuranEngine
+//
+//  Created by Mohannad Hassan on 18/02/2025.
+//
+
+import Foundation
+import XCTest
+
+final class SynchronizedPageBookmarkPersistenceTests: XCTestCase {
+    
+}

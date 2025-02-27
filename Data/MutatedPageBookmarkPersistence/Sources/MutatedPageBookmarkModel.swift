777361ee0a50276e58094f1b2ea21492758834c0
Mohannad A. Hassan <mohannad.hassan.90@gmail.com>
2025-02-27 08:02:07 +0200

    Documentation

--- Data/MutatedPageBookmarkPersistence/Sources/MutatedPageBookmarkModel.swift
+++ Data/MutatedPageBookmarkPersistence/Sources/MutatedPageBookmarkModel.swift
@@ -7,14 +7,30 @@
 
 import Foundation
 
+/// Represents a page bookmark with mutation information.
+///
+/// This model is used to represent a bookmark that has been created or mutated locally. The model
+/// may reference a remote bookmark that has been synced with the upstream server, in which case,
+/// `remoteID` will be the ID of that bookmark. This means that the bookmark has been deleted
+/// locally, but the deletion hasn't been marked as synced yet.
+///
+/// See `MutatedPageBookmarkPersistence` for more information regarding
+/// the relationship between the local mutated bookmarks and the upstream-synced bookmarks.
 public struct MutatedPageBookmarkModel {
     public enum Mutation {
         case created
         case deleted
     }
 
+    /// If `nil`, then this is a local bookmark that hasn't been synced upstream. Otherwise, this
+    /// is the remote ID of the upstream bookmark.
     public let remoteID: String?
     public let page: Int
     public let modificationDate: Date
+
+    /// Represents the kind of mutation that casued this bookmark's current state.
+    ///
+    /// If `remoteID` is `nil`, then this will be `.created`. For remote bookmarks, this will be
+    /// `.deleted`.
     public let mutation: Mutation
 }
--- Data/MutatedPageBookmarkPersistence/Sources/MutatedPageBookmarkPersistence.swift
+++ Data/MutatedPageBookmarkPersistence/Sources/MutatedPageBookmarkPersistence.swift
@@ -13,6 +13,18 @@ public enum MutatedPageBookmarkPersistenceError: Error {
     case illegalState(reason: String, page: Int)
 }
 
+/// Provides access for a mutable page bookmark persistence.
+///
+/// This persistence layer is aware of the difference between downstream and upstream-synced bookmarks. It can mark
+/// a mutation record for an upstream-synced bookmark, while providing access to create and remove local-unsynced
+/// bookmarks. If used in an unconnected state, this persistence can be can be seen as an local bookmarks persistence,
+/// without reference to upstream resources.
+///
+/// The repository guarantees that it can only hold a single *created* bookmark for a page. It may hold a record of a
+/// deleted upstream bookmark for a page holding a record for a created local unsynced bookmark. It's the responsibility
+/// of the client of this persistence to guarantee any further data requirements with the upstream resources.
+///
+/// See `MutatedPageBookmarkModel` for more information on the state of the returend objects.
 public protocol MutatedPageBookmarkPersistence {
     func bookmarksPublisher() throws -> AnyPublisher<[MutatedPageBookmarkModel], Never>
 
@@ -20,9 +32,22 @@ public protocol MutatedPageBookmarkPersistence {
 
     func bookmarkMutations(page: Int) async throws -> [MutatedPageBookmarkModel]
 
+    /// Creates a new local bookmark for the given page.
+    ///
+    /// - throws `MutatedPageBookmarkPersistenceError.bookmarkAlreadyExists`
     func createBookmark(page: Int) async throws
 
+    /// Signals the removal of the bookmark for the given page.
+    ///
+    /// If `remoteID` is given, this marks the record for deletion on the associated upstream resource.
+    /// If `remoteID` is `nil`, this will remove the records of the bookmark of the given page.
+    ///
+    /// - throws `MutatedPageBookmarkPersistenceError.illegalState` if
+    /// `remoteID` is nil, and there's no local  bookmark for that page.
     func removeBookmark(page: Int, remoteID: String?) async throws
 
+    /// Clears all records.
+    ///
+    /// Should only be called after a sync operation with the upstream source is done.
     func clear() async throws
 }

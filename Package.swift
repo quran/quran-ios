7235da99508659a6643fb754eb9ff584ba7637c6
Mohamed Afifi <mohamede1945@gmail.com>
2025-12-02 14:45:53 -0500

    Replace migration screen xib with SwiftUI

--- Package.swift
+++ Package.swift
@@ -55,7 +55,6 @@ let package = Package(
         .package(url: "https://github.com/GenericDataSource/GenericDataSource", from: "3.1.3"),
         .package(url: "https://github.com/SvenTiigi/WhatsNewKit", from: "1.3.7"),
         .package(url: "https://github.com/mohamede1945/Popover", branch: "master"),
-        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView", from: "5.0.0"),
 
         // Testing
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.9.0"),
@@ -559,7 +558,6 @@ private func featuresTargets() -> [[Target]] {
             "AppMigrator",
             "ReciterService",
             "Utilities",
-            "NVActivityIndicatorView",
             "NoorUI",
         ]),
 

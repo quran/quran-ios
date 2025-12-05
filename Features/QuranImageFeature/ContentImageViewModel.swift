a0fec9ba342c24bd38c1ff4bdfe1e4b51ba67e7e
Mohamed Afifi <mohamede1945@users.noreply.github.com>
2025-12-05 18:13:38 -0500

    Render hafs_1440 with original style instead of tinting (#726)

--- Features/QuranImageFeature/ContentImageViewModel.swift
+++ Features/QuranImageFeature/ContentImageViewModel.swift
@@ -51,7 +51,7 @@ class ContentImageViewModel: ObservableObject {
     @Published var imageFrame: CGRect = .zero
 
     var imageRenderingMode: QuranThemedImage.RenderingMode {
-        reading == .tajweed ? .invertInDarkMode : .tinted
+        [.tajweed, .hafs_1440].contains(reading) ? .invertInDarkMode : .tinted
     }
 
     var decorations: ImageDecorations {

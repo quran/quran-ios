ede7049ca5888a50c33dc5ac4afad6163a2f1805
Mohamed Afifi <mohamede1945@gmail.com>
2026-05-02 17:09:17 -0400

    style: format playback speed helper

--- UI/NoorUI/Formatters/PlaybackSpeed.swift
+++ UI/NoorUI/Formatters/PlaybackSpeed.swift
@@ -9,7 +9,7 @@ import Foundation
 import Localization
 
 public enum PlaybackSpeed {
-  public static let supportedRates: [Float] = [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
+    public static let supportedRates: [Float] = [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
 
     private static let formatter: NumberFormatter = {
         let formatter = NumberFormatter()

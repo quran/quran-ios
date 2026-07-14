1bb3550af414a176854aaafe7939930f2e3fd01a
Mohamed Afifi <mohamede1945@gmail.com>
2026-07-13 20:07:29 -0400

    build: update mobile-sync-spm to 0.1.16
    Preserve immutable iterator compatibility while retaining the Swift-native coroutine boundary.

--- Package.swift
+++ Package.swift
@@ -65,7 +65,7 @@ let package = Package(
         .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "1.0.0"),
 
         // Sync
-        .package(url: "https://github.com/quran/mobile-sync-spm.git", from: "0.1.15"),
+        .package(url: "https://github.com/quran/mobile-sync-spm.git", from: "0.1.16"),
 
     ], targets: validated(targets) + [testTargetLinkingAllPackageTargets(targets)]
 )
